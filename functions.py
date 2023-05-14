02a54fc88512bcd4c8cd0080c5b6653540f96129
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-14 10:27:42 -0600

    Incorporated sqlite3 instead of saving data to csv file. Scrapes test website every 2 seconds and sends an email only when new event found. Saves only unique values to database. Not included in the lesson, but added because I wanted to: Added subject line to email with event info. Also added event info in the text of the email.

--- functions.py
+++ functions.py
@@ -4,11 +4,17 @@ import ssl
 import os
 import smtplib
 import time
+import sqlite3
 
+# URL where I continuously fetch data
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 '
                          '(KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# Establish a connection to the sqlite3 db
+connection = sqlite3.connect("data.db")
+
 
 def scrape(url):
     """Scrape the page source from the URL"""
@@ -23,16 +29,24 @@ def extract(source):
     return value
 
 
-def store(extacted):
-    with open("data.txt", "a") as file:
-        file.write(extracted + "\n")
+def store(extracted):
+    row = extracted.split(",")
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", (band, city, date))
+    connection.commit()
 
 
 def read(extracted):
-    with open("data.txt", "r") as file:
-        return file.read()
+    row = extracted.split(",")
+    band, city, date = row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
-def send_email(message):
+
+def send_email(subject, message, extracted_value):
     host = "smtp.gmail.com"
     port = 465
 
@@ -44,18 +58,25 @@ def send_email(message):
 
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
-        server.sendmail(username, receiver, message)
+        print("Logged in successfully")
+        subject_line = f"New event found: {extracted_value}"
+        email_message = f"{message}\n\nHere's the event: {extracted_value}"
+        server.sendmail(username, receiver, f"Subject: {subject_line}\n\n{email_message}")
+        print("Email sent successfully")
 
 
 if __name__ == "__main__":
     while True:
         scraped = scrape(URL)
         extracted = extract(scraped)
-        print(extracted)
-
-        content = read(extracted)
+        print("extracted:", extracted)
         if extracted != "No upcoming tours":
-            if extracted not in content:
+            row = read(extracted)
+            # Checks to see if event already exists in the table
+            if not row:
                 store(extracted)
-                send_email(message = "Hey new event was found!")
-    time.sleep(2)
+                extracted_value = ", ".join(extracted.split(","))
+                message = "Hey new event was found!"
+                subject = "New event alert!"
+                send_email(subject, message, extracted_value)
+        time.sleep(2)
