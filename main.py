fbfb1aae1e55c09c51c5b8631b84b9787861c7f3
Ashley Yaner <ashley.yaner@fractureid.com>
2023-05-14 10:29:37 -0600

    Updated main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from functions import scrape, extract, store, read, send_email
+import time
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 while True:
     scraped = scrape(URL)
     extracted = extract(scraped)
-    print(extracted)
-
-    content = read(extracted)
+    print("extracted:", extracted)
     if extracted != "No upcoming tours":
-        if extracted not in content:
+        row = read(extracted)
+        # Checks to see if event already exists in the table
+        if not row:
             store(extracted)
-            send_email(message="Hey new event was found!")
-time.sleep(2)
+            extracted_value = ", ".join(extracted.split(","))
+            message = "Hey new event was found!"
+            subject = "New event alert!"
+            send_email(subject, message, extracted_value)
+    time.sleep(2)
